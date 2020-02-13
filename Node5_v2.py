f7fa4755682624e710d5c434ba4798084a873193
IyerAkhilesh <43975500+IyerAkhilesh@users.noreply.github.com>
2020-02-12 20:20:46 -0500

    The second modified upload
    There is an added authenticator and updated power_raisers

--- Node5_v2.py
+++ Node5_v2.py
@@ -4,12 +4,12 @@ class Node5:
 
     worker_node_dict = {
         "node1": 50200,
-        "node2": 20500,
-        "node3": 21000,
-        "node4": 21500,
-        "node5": 22000,
-        "node6": 22500,
-        "node7": 23000
+        "node2": 50500,
+        "node3": 51000,
+        "node4": 51500,
+        "node5": 52000,
+        "node6": 52500,
+        "node7": 53000
     }
     transactions = []
     bch = None
@@ -90,7 +90,6 @@ class Node5:
             print("CANNOT MINE BLOCK!")
             return
 
-
     def print_blockchain(self):
         self.bch.print_block_chain()
 
@@ -112,13 +111,113 @@ class Node5:
             self.transactions.append(self.block_table+"->"+worker_node_id+"->"+str(exponents))
             sender_socket.send(dump_var)
             dump_var = sender_socket.recv(1024)
+
+            sender_socket.send(bytes("ack".encode()))
+            registration_result = sender_socket.recv(512)
+            sender_socket.send(bytes("ack".encode()))
             powers = pickle.loads(dump_var)
             self.transactions.append(worker_node_id+"->"+self.block_table+"->"+str(powers))
             print(self.transactions)
             sender_socket.close()
+
+            if "True" in str(registration_result.decode()):
+                return "Successful!"
+            else:
+                return "Unsuccessful!"
+
         except ConnectionRefusedError as cr:
             print(cr)
 
+    # the login function with parameters as the IDs of nodes that will serve the functions
+    # of containers and authenticator FaaS servers
+    def login(self, container1,  container2, container3, container4, authenticator1, authenticator2):
+        from BlockchainCode import SumProductSum as prng
+
+        username = input("Enter the username: ")
+        password = input("Enter the password: ")
+        exponents = prng.get_factors(username, password)
+        print("Exponents array: ", exponents)
+        # exponents = str(exponents[0]) + "," + str(exponents[1]) + "," + str(exponents[2])
+
+        # send first value to container one and get back the exponent
+        X = self.send_to_container(container1, str(exponents[0]))
+
+        # send second value to container two and get back the exponent
+        Y = self.send_to_container(container2, str(exponents[1]))
+        print("Powers returned by container servers: ", X, Y)
+
+        R_ONE = self.send_to_authenticator(authenticator1, X, exponents[2], exponents[0])
+
+    def send_to_authenticator(self,container_id, power, exponent1, exponent2):
+        import socket
+        import pickle
+
+        dump_var = pickle.dumps(power)
+        try:
+            sender_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sender_socket.connect(("127.0.0.1", self.worker_node_dict[container_id]))
+
+            self.transactions.append(self.block_table + "->" + container_id + "->" + str(power))
+            sender_socket.send(dump_var)
+
+            # what is received is challenge nb1
+            dump_var = sender_socket.recv(1024)
+            nb1 = pickle.loads(dump_var)
+            print("Challenge: ", nb1)
+
+            # what is calculated is response r1
+            r1 = int(nb1) * exponent1 + exponent2
+            print("Response: ", r1)
+            dump_var = pickle.dumps(r1)
+            sender_socket.send(dump_var)
+
+            transaction_result = sender_socket.recv(1024)
+            sender_socket.send(bytes("ack".encode()))
+
+            self.transactions.append(container_id + "->" + self.block_table + "->" + str(power))
+            sender_socket.close()
+            print(self.transactions)
+
+            if "True" in str(transaction_result.decode()):
+                print("Hello there! Your chances of logging in successfully are 50% now!")
+                return True
+            else:
+                print("I am sorry! You can't login now!")
+                return False
+        except ConnectionRefusedError as cr:
+            print(cr)
+
+    def send_to_container(self, container_id, exponent):
+        import socket
+        import pickle
+
+        print("Exponent sent to "+container_id, exponent)
+        dump_var = pickle.dumps(exponent)
+
+        try:
+            sender_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print("Port ", self.worker_node_dict[container_id])
+            sender_socket.connect(("127.0.0.1", self.worker_node_dict[container_id]))
+            self.transactions.append(self.block_table + "->" + container_id + "->" + str(exponent))
+            sender_socket.send(dump_var)
+            dump_var = sender_socket.recv(1024)
+
+            sender_socket.send(bytes("ack".encode()))
+            transaction_result = sender_socket.recv(512)
+            sender_socket.send(bytes("ack".encode()))
+            power = pickle.loads(dump_var)
+            self.transactions.append(container_id + "->" + self.block_table + "->" + str(power))
+            sender_socket.close()
+            print(self.transactions)
+
+            if "True" in str(transaction_result.decode()):
+                print("Hello there! This is your first transaction!")
+                return str(power)
+            else:
+                print("Congratulations: You have logged in more than once!")
+                return str(power)
+        except ConnectionRefusedError as cr:
+            print(cr)
 
 n1 = Node5()
 # Access and insert the first block in the database "user_information" and table node5
@@ -140,13 +239,11 @@ try:
 
         else:
             # Registration
-            registered_flag = n1.register_node("node1")
-            print("Registration status: ",registered_flag)
-            # n1.mine_block(["username: people", "password: areAlive"])
-            # n1.mine_block(["Akhilesh: Iyer", "Vignesh: Iyer", "Shikha: Doshi"])
-            # n1.mine_block(["Grocery: 50", "Travel: 104"])
-            # n1.mine_block(["Grocery: 50", "Travel: 101"])
-            # n1.mine_block(["Peeping: Tom", "Running: Jerry"])
+            # registered_flag = n1.register_node("node1")
+            # print("Registration status: ",registered_flag)
+
+            # Login
+            n1.login("node1", "node3", "", "", "node6", "")
 
     conn.commit()
 finally:
