cd7fe6f9d4025d3a0fe86bd4cf85035baa8beb4c
/-\khile$h <iyerakhileshk@gmail.com>
2025-11-23 13:27:28 +0000

    Made the code backwards compatible with Python 3.0+

--- SumProductSum.py
+++ SumProductSum.py
@@ -1,5 +1,9 @@
 import math
 
+# Import the below libraries if you are using a version of Python less than 3.8
+# import functools
+# import operator
+
 class FactorService:
     # Defined as a class constant so it isn't recreated on every function call
     CHAR_MAP = {
@@ -23,7 +27,10 @@ class FactorService:
         if n < 2: return []
         
         factors = set()
-        # We only need to check up to the square root of n
+        # Use the sqrt() function if you are using a version of Python less than 3.8 
+        # for i in range(2, int(math.sqrt(n)) + 1):
+
+        # Else, use isqrt() - We only need to check up to the square root of n
         for i in range(2, int(math.isqrt(n)) + 1):
             if n % i == 0:
                 factors.add(i)
@@ -48,8 +55,16 @@ class FactorService:
         val_sum = sum(values)
 
         # 2. Product of characters at odd indices
-        # Slicing [1::2] grabs odd indices. math.prod is faster than reduce.
+
         odd_index_values = values[1::2]
+
+        # Use the reduce() function if you are using a version of Python less than 3.8 
+        # if odd_index_values:
+        #     val_prod = functools.reduce(operator.mul, odd_index_values, 1)
+        # else:
+        #     val_prod = 0
+
+        # Else, use prod() - Slicing [1::2] grabs odd indices. math.prod is faster than reduce.
         val_prod = math.prod(odd_index_values) if odd_index_values else 0
 
         # Get factors for both
@@ -90,7 +105,12 @@ class FactorService:
         # math.floor(len * 0.65) - 1 is roughly index at 65%
         p_factor_final = cls.extract_indices(p_factors, 0.65)
         
-        # Output results
+
+        # # Use the print("".format({var})) function if you are using a version of Python less than 3.6
+        # print("User Factors: {}, {}".format(u_factor_one, u_factor_two))
+        # print("Pass Factor: {}".format(p_factor_final))
+
+        # Else, use print(f"{var}") - Output results
         print(f"User Factors: {u_factor_one}, {u_factor_two}")
         print(f"Pass Factor: {p_factor_final}")
         
